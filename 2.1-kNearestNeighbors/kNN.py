f7971d6adb8d41f80051a6d6cb2e09a302541cbe
Kevin Herr <kherr9@gmail.com>
2017-04-29 09:56:00 -0500

    comments

--- 2.1-kNearestNeighbors/kNN.py
+++ 2.1-kNearestNeighbors/kNN.py
@@ -8,15 +8,18 @@ def createDataSet():
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
+    # calc distance (Euclidian)
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()     
+    sortedDistIndicies = distances.argsort()
+    # voting with lowest k distances  
     classCount={}          
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    # sort dictionary
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
